9c7eeac61eae168a7f2d635774b4be49a7540aaf
Jason Deitz <thewanderer@Jasons-MacBook-Pro.local>
2021-07-07 20:38:39 -0700

    basic nav

---
+++ hand_in_hand/hih/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('clients/portal/', views.clientportal, name='clientportal'),
+  path('clients/portal/', views.clientportal, name='clientportal'),
+  path('prprofile/', views.providerprofile, name='providerprofile'),
+  path('prclients/', views.yourclients, name='clients'),
+  path('clprofile/', views.clientprofile, name='clientprofile'),
+  path('clproviders/', views.yourproviders, name='providers'),
+  path('yourforms/', views.yourforms, name='yourforms'),
+]
--- hand_in_hand/hih/views.py
+++ hand_in_hand/hih/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def providerprofile(request):
+  return render(request, 'providerprofile.html')
+
+def yourclients(request):
+  return render(request, 'yourclients.html')
+
+def clientprofile(request):
+  return render(request, 'clientprofile.html')
+
+def yourproviders(request):
+  return render(request, 'yourproviders.html')
+
+def yourforms(request):
+  return render(request, 'yourforms.html')
+
+def clientportal(request):
+  return render(request, 'client/portal.html')
+
+def providerportal(request):
+  return render(request, 'provider/portal.html')
