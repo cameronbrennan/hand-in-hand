3222c5a4f32ccf843c26cf0101bedbbe70b438fb
Jason Deitz <thewanderer@Jasons-MacBook-Pro.local>
2021-07-09 22:23:21 -0700

    adds Client, Provider amd Assignemtn starter Model, adds admin functionality to models on superuser control

--- hand_in_hand/hih/admin.py
+++ hand_in_hand/hih/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Provider, Assignment
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Provider)
+admin.site.register(Assignment)
+# admin.site.register(User)  
--- hand_in_hand/hih/models.py
+++ hand_in_hand/hih/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    pronouns = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    phone = models.CharField(max_length=100)
+    age = models.IntegerField()
+    location = models.IntegerField() #zipcode? can change to str later
+    def __str__(self):
+        return self.name
+    
+class Provider(models.Model):
+    name = models.CharField(max_length=100)
+    pronouns = models.CharField(max_length=100)
+    email = models.CharField(max_length=150)
+    phone = models.CharField(max_length=100)
+    about = models.TextField(max_length=500)
+    certification = models.CharField(max_length=150)
+    certnum = models.CharField(max_length=100)
+    location = models.IntegerField() #zipcode? can change to str later
+    def __str__(self):
+        return self.name
+    
+
+class Assignment(models.Model):
+    date = models.DateField('date assigned')
