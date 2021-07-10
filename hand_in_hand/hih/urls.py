38e882aafa336c45543bf3b18d57f2720195bcc5
Jason Deitz <thewanderer@Jasons-MacBook-Pro.local>
2021-07-10 15:39:19 -0700

    adds login route and button

--- hand_in_hand/hih/urls.py
+++ hand_in_hand/hih/urls.py
@@ -7,19 +7,20 @@ urlpatterns = [
   path('successfullogin/', views.successfullogin, name='successfullogin'),
   path('accounts/signup/', views.signup, name='signup'),
   path('accounts/login/', views.login, name='login'),
+  path('login',views.login_view, name = 'login '),
   
   #### CLIENT URLS
   path('clients/portal/', views.clientportal, name='clientportal'),
   path('clprofile/', views.clientprofile, name='clientprofile'),
   path('clproviders/', views.yourproviders, name='providers'),
   path('accounts/clientsignup/', views.clientsignup, name='clientsignup'),
-  # path('accounts/login/', views.clientlogin, name='login'),
+  path('registration/clientlogin/', views.clientlogin, name='clientlogin'),
   
   ##### PROVIDER URLS
   path('providers/portal/', views.providerportal, name='providerportal'),
   path('prclients/', views.yourclients, name='clients'),
   path('prprofile/', views.providerprofile, name='providerprofile'),
   path('accounts/providersignup/', views.providersignup, name='providersignup'),
-  # path('accounts/login/', views.providerlogin, name='login'),
+  path('registration/providerlogin/', views.providerlogin, name='providerlogin'),
 
 ]
